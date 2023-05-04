c0f4831be63e1c89f0d7f41a5c33115bdb239187
Pipe Alfaro <piperfe21@gmail.com>
2023-05-04 09:51:11 -0400

    [MOVE_ALL_ZEROS_TO_THE_LEFT] add zero unique element in an array test case

--- test/test_shift_all_the_zeros_to_the_left.py
+++ test/test_shift_all_the_zeros_to_the_left.py
@@ -4,6 +4,7 @@ from src.shift_all_the_zeros_to_the_left import move_zeros_to_the_left
 
 
 class ShiftAllTheZerosToTheLeftTestCase(unittest.TestCase):
+
     def test_move_one_zero_to_the_left_in_the_array(self):
         array_with_one_zero = [1, 2, 2, 0]
 
@@ -32,3 +33,9 @@ class ShiftAllTheZerosToTheLeftTestCase(unittest.TestCase):
 
         self.assertEqual([0, 0, 0, 2, 1, 2], array_with_zeros_to_the_left)
 
+    def test_do_not_move_the_zeros_in_an_array_with_one_zero_element(self):
+        array_with_zero = [0]
+
+        array_with_zeros_to_the_left = move_zeros_to_the_left(array_with_zero)
+
+        self.assertEqual([0], array_with_zeros_to_the_left)
