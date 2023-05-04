f570dcc6be0a33a679a53fa234c187bac74823f8
Pipe Alfaro <piperfe21@gmail.com>
2023-05-04 11:15:19 -0400

    [REVERSE ORDER OF STRING | REFACTOR] replace for by while and assing a var indicated the value of the character in the specific array index position

--- src/reverse_order_of_string.py
+++ src/reverse_order_of_string.py
@@ -1,17 +1,20 @@
 def reverse_order(word):
     word_in_array = convert_in_array_of_chars(word)
+
     init_to_end = 0
     end_to_init = len(word_in_array) - 1
 
-    for character in word_in_array:
-        if init_to_end >= end_to_init:
-            return convert_in_string(word_in_array)
+    while init_to_end < end_to_init:
+        init_to_end_character = word_in_array[init_to_end]
+        end_to_init_character = word_in_array[end_to_init]
 
-        word_in_array[init_to_end] = word_in_array[end_to_init]
-        word_in_array[end_to_init] = character
+        word_in_array[init_to_end] = end_to_init_character
+        word_in_array[end_to_init] = init_to_end_character
 
         init_to_end += 1
         end_to_init -= 1
+
+    return convert_in_string(word_in_array)
 
 
 def convert_in_string(array_of_chars):
