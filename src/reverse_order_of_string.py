abafa021ddf5e535b1cee363c63ffd4ad13eba41
Pipe Alfaro <piperfe21@gmail.com>
2023-05-05 10:52:22 -0400

    [REVERSE ORDER OF STRING | REFACTOR] rename meaningful vars of the algorithm

--- src/reverse_order_of_string.py
+++ src/reverse_order_of_string.py
@@ -1,18 +1,19 @@
 def reverse_order(word):
     word_in_array = convert_in_array_of_chars(word)
 
-    init_to_end = 0
-    end_to_init = len(word_in_array) - 1
+    init_to_end_index = 0
+    length_word_in_array = len(word_in_array) - 1
+    end_to_init_index = length_word_in_array
 
-    while init_to_end < end_to_init:
-        init_to_end_character = word_in_array[init_to_end]
-        end_to_init_character = word_in_array[end_to_init]
+    while init_to_end_index < end_to_init_index:
+        init_to_end_character = word_in_array[init_to_end_index]
+        end_to_init_character = word_in_array[end_to_init_index]
 
-        word_in_array[init_to_end] = end_to_init_character
-        word_in_array[end_to_init] = init_to_end_character
+        word_in_array[init_to_end_index] = end_to_init_character
+        word_in_array[end_to_init_index] = init_to_end_character
 
-        init_to_end += 1
-        end_to_init -= 1
+        init_to_end_index += 1
+        end_to_init_index -= 1
 
     return convert_in_string(word_in_array)
 
